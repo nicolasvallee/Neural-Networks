abb00d5b83472d16254bebe4adf89a0a2f27e792
nicolas <nicoval2001@hotmail.fr>
2020-07-27 14:49:06 +0200

    91% in 3 minutes
    Can go up to 95% if left longer (30 epochs..)

--- constants.hpp
+++ constants.hpp
@@ -8,12 +8,12 @@ const int NB_LAYERS = 3; //Number of layers of nodes, including the input and ou
 
 const int picture_size = 28 * 28;
 const int NB_NODES_PER_LAYER[NB_LAYERS] = 
-{picture_size, 30, 10};
+{picture_size, 20, 10};
 
-const int training_set_size = 1000;
+const int training_set_size = 60000;
 const int test_set_size = 5000;
 
-const int nbEpochs = 30;
+const int nbEpochs = 3;
 const int batchSize = std::min(training_set_size, 10);
 
 
